9bfd99da0392758447d5d3896fb92f778c9d03cc
MedMejri1 <mejrief@gmail.com>
2024-12-23 11:10:48 +0100

    done

--- components/data/portfolio-data.jsx
+++ components/data/portfolio-data.jsx
@@ -7,41 +7,41 @@ import image6 from "../../public/assets/img/portfolio/portfolio-1.jpg";
 
 const portfolioData = [
     {        
-        id: 'real-time-monitoring',
-        title: 'Real-Time Monitoring',
-        subtitle: 'Network Analysis',
+        id: 'makeup',
+        title: 'Leaders Makeup',
+        subtitle: '',
         image: image1,        
     },
     {        
-        id: 'gateway-integration',
-        title: 'Gateway Integration',
-        subtitle: 'Hosting Solution',
+        id: 'leadersimmo',
+        title: 'Leaders Immobiler',
+        subtitle: '',
         image: image2,        
     },
     {        
-        id: 'management-system',
-        title: 'Management System',
-        subtitle: 'Data Recovery',
+        id: 'leadersbuilding',
+        title: 'Leaders Building',
+        subtitle: '',
         image: image3,        
     },
-    {        
-        id: 'innovative-solutions',
-        title: 'Innovative Solutions',
-        subtitle: 'Risk Management',
-        image: image4,        
-    },
-    {        
-        id: 'it-success-stories',
-        title: 'IT Success Stories',
-        subtitle: 'System Integration',
-        image: image5,        
-    },
-    {        
-        id: 'growth-technology',
-        title: 'Growth Technology',
-        subtitle: 'Software Planning',
-        image: image6,        
-    },
+    // {        
+    //     id: 'innovative-solutions',
+    //     title: 'Innovative Solutions',
+    //     subtitle: 'Risk Management',
+    //     image: image4,        
+    // },
+    // {        
+    //     id: 'it-success-stories',
+    //     title: 'IT Success Stories',
+    //     subtitle: 'System Integration',
+    //     image: image5,        
+    // },
+    // {        
+    //     id: 'growth-technology',
+    //     title: 'Growth Technology',
+    //     subtitle: 'Software Planning',
+    //     image: image6,        
+    // },
 ];
 
 export default portfolioData;
--- components/pages/portfolio/portfolio-details/portfolio-details.jsx
+++ components/pages/portfolio/portfolio-details/portfolio-details.jsx
@@ -2,7 +2,7 @@ import icon from '../../../../public/assets/img/icon/project-details-icon.png';
 import image1 from '../../../../public/assets/img/portfolio/project-details.png';
 import image2 from '../../../../public/assets/img/portfolio/project-details-2.png';
 
-const PortfolioDetailsMain = ({ singleData }) => {
+const PortfolioDetailsMain = ({singleData}) => {
     return (
         <div className="project__details section-padding">
             <div className="container">
@@ -38,16 +38,14 @@ const PortfolioDetailsMain = ({ singleData }) => {
                             <img src={singleData.image.src} alt="image" />
                             <div className="project-info">
                                 <div className="project-info-top">
-                                    <h4>Détails du Projet</h4>
+                                    <h4>Project Details</h4>
                                 </div>
                                 <ul>
-                                    <li>Nom :<span>Leaders Makeup</span></li>
-                                    <li>Description :<span>Développement de site web et CRM pour gestion du stock</span></li>
-                                    <li>Charte Graphique :<span>Incluse</span></li>
-                                    <li>Lien du site web :<span><a href="https://wakeup-cosmetics.tn/" target="_blank" rel="noopener noreferrer">wakeup-cosmetics.tn</a></span></li>
-                                    <li>Date :<span>23 Décembre 2023</span></li>
-                                    <li>Tags :<span>Web, CRM, Gestion de Stock</span></li>
-                                    <li>Valeur :<span className="value">Sur demande</span></li>
+                                    <li>Name:<span>{singleData.title}</span></li>
+                                    <li>Author:<span>Rajin Saleh</span></li>
+                                    <li>Date:<span>23 December,2023</span></li>
+                                    <li>Tags:<span>DataMasters</span></li>
+                                    <li>Value:<span className="value">$ 240</span></li>
                                     <li className="project-rating">
                                         <i className="fas fa-star"></i>
                                         <i className="fas fa-star"></i>
@@ -58,32 +56,26 @@ const PortfolioDetailsMain = ({ singleData }) => {
                                 </ul>
                             </div>
                         </div>
-
                     </div>
                     <div className="col-xl-8">
                         <div className="project__details-content">
                             <div className="project__details-content-mid">
-                                <p>Leaders Makeup est une marque ambitieuse qui a entrepris un projet stratégique visant à transformer son infrastructure digitale. Le projet inclut le développement d'un site web moderne et d'un CRM personnalisé pour la gestion efficace des stocks et des opérations commerciales.</p>
-                                <h3>Une Solution Digitale Complète</h3>
-                                <p>Le site web conçu pour Leaders Makeup offre une expérience utilisateur intuitive et met en valeur leur gamme de produits cosmétiques. Parallèlement, le CRM développé permet une gestion centralisée des stocks, offrant des outils d’analyse, de suivi des commandes et de gestion des approvisionnements.</p>
-                                <p>Le projet inclut également la création d’une charte graphique sur mesure, garantissant une cohérence visuelle et une identité forte pour la marque sur toutes ses plateformes digitales.</p>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galltype and scrambled it to make a type specimen book. It has survived not only five centuries tinto electronic typesetting remaining essentially unchanged Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been </p>
+                                <h3>Enabling Seamless IT Experiences</h3>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galltype and scrambled it to make a type specimen book</p>
+                                <p>. It has survived not only five centuries tinto electronic typesetting remaining essentially unchanged Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been </p>
                             </div>
                         </div>
                     </div>
                     <div className="project__details-images">
-                        <img src={image1.src} alt="capture d'écran du site web" />
-                        <img src={image2.src} alt="interface CRM" />
-                    </div>
-                    <div className="project__details-images">
-                        <img src={image1.src} alt="capture d'écran du site web" />
-                        <img src={image2.src} alt="interface CRM" />
+                        <img src={image1.src} alt="image" />
+                        <img src={image2.src} alt="image" />
                     </div>
                     <div className="project__details-content">
                         <div className="project__details-content-bottom">
-                            <p>Grâce à ce projet, Leaders Makeup renforce sa position sur le marché des cosmétiques en Tunisie. Le site web et le CRM apportent des solutions fiables pour répondre aux besoins de leur clientèle et faciliter la gestion interne de leurs opérations. Ce projet incarne l’innovation et la performance au service de la beauté.</p>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industr standard dummy text ever since the 1500s, when an unknown printer took a galltype and scrambled it to make a type specimen book. It has survived a only five centuries tinto electronic typesetting remaining essentially unchanged Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been </p>
                         </div>
                     </div>
-
                 </div>
             </div>
         </div>

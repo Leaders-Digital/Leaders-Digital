bb085475a8b940bd3ac141181c776bbd90e4edb7
Mohamed Mejri <mejrief@gmail.com>
2025-02-08 19:00:14 +0100

    done

--- components/layout/headers/header-menu.jsx
+++ components/layout/headers/header-menu.jsx
@@ -22,9 +22,9 @@ const MainMenu = () => {
                         <li className="menu-item-has-children"><Link href="#">Développement Mobile<i className="fas fa-angle-right"></i></Link>
                             <ul className="sub-menu">
                                 <li><Link href="/mobile">Applications mobiles iOS et Android</Link></li>
-                                <li><Link href="/team-two">Création d’applications PWA</Link></li>
-                                <li><Link href="/team-two">Refonte et maintenance d’applications mobiles</Link></li>
-                                <li><Link href="/team-two">Tests et déploiement sur les stores</Link></li>
+                                <li><Link href="#">Création d’applications PWA</Link></li>
+                                <li><Link href="#">Refonte et maintenance d’applications mobiles</Link></li>
+                                <li><Link href="#">Tests et déploiement sur les stores</Link></li>
                              
 
                             </ul>
@@ -32,12 +32,12 @@ const MainMenu = () => {
 
                         <li className="menu-item-has-children"><Link href="#">Conception Graphique<i className="fas fa-angle-right"></i></Link>
                             <ul className="sub-menu">
-                                <li><Link href="/team">Identité visuelle & branding</Link></li>
-                                <li><Link href="/team-two">Design de supports imprimés</Link></li>
-                                <li><Link href="/team-two">Web design & UI/UX Design</Link></li>
-                                <li><Link href="/team-two">Création de visuels pour réseaux sociaux</Link></li>
-                                <li><Link href="/team-two">Motion design et animations vidéo</Link></li>
-                                <li><Link href="/team-two">Packaging design</Link></li>
+                                <li><Link href="#">Identité visuelle & branding</Link></li>
+                                <li><Link href="#">Design de supports imprimés</Link></li>
+                                <li><Link href="#">Web design & UI/UX Design</Link></li>
+                                <li><Link href="#">Création de visuels pour réseaux sociaux</Link></li>
+                                <li><Link href="#">Motion design et animations vidéo</Link></li>
+                                <li><Link href="#">Packaging design</Link></li>
 
 
                             </ul>
@@ -45,12 +45,12 @@ const MainMenu = () => {
 
                         <li className="menu-item-has-children"><Link href="#">Référencement SEO & Marketing Digital<i className="fas fa-angle-right"></i></Link>
                             <ul className="sub-menu">
-                                <li><Link href="/team">Audit SEO</Link></li>
-                                <li><Link href="/team-two">Optimisation on-page et off-page</Link></li>
-                                <li><Link href="/team-two">Web design & UI/UX Design</Link></li>
-                                <li><Link href="/team-two">Rédaction de contenu optimisé SEO</Link></li>
-                                <li><Link href="/team-two">Stratégie marketing sur les réseaux sociaux</Link></li>
-                                <li><Link href="/team-two">Email marketing</Link></li>
+                                <li><Link href="#">Audit SEO</Link></li>
+                                <li><Link href="#">Optimisation on-page et off-page</Link></li>
+                                <li><Link href="#">Web design & UI/UX Design</Link></li>
+                                <li><Link href="#">Rédaction de contenu optimisé SEO</Link></li>
+                                <li><Link href="#">Stratégie marketing sur les réseaux sociaux</Link></li>
+                                <li><Link href="#">Email marketing</Link></li>
 
 
                             </ul>
@@ -58,9 +58,9 @@ const MainMenu = () => {
 
                         <li className="menu-item-has-children"><Link href="#">Gestion des Réseaux Sociaux<i className="fas fa-angle-right"></i></Link>
                             <ul className="sub-menu">
-                                <li><Link href="/team">Community management</Link></li>
-                                <li><Link href="/team-two">Création de contenu visuel et rédactionnel</Link></li>
-                                <li><Link href="/team-two">Stratégie de contenu et planning éditorial</Link></li>
+                                <li><Link href="#">Community management</Link></li>
+                                <li><Link href="#">Création de contenu visuel et rédactionnel</Link></li>
+                                <li><Link href="#">Stratégie de contenu et planning éditorial</Link></li>
 
 
                             </ul>
@@ -68,9 +68,9 @@ const MainMenu = () => {
 
                         <li className="menu-item-has-children"><Link href="#">Photographie et Vidéographie<i className="fas fa-angle-right"></i></Link>
                             <ul className="sub-menu">
-                                <li><Link href="/team">Shooting photo professionnel</Link></li>
-                                <li><Link href="/team-two">Production de vidéos d’entreprise</Link></li>
-                                <li><Link href="/team-two">Montage et retouche vidéo</Link></li>
+                                <li><Link href="#">Shooting photo professionnel</Link></li>
+                                <li><Link href="#">Production de vidéos d’entreprise</Link></li>
+                                <li><Link href="#">Montage et retouche vidéo</Link></li>
 
 
                             </ul>
